ed539260d10112a52cb2768275de37b78d1b3eb1
IraildoF <franciscoiraildof@gmail.com>
2026-09-09 19:36:31 -0300

    Add files via upload

--- mobile/sw.js
+++ mobile/sw.js
@@ -1,5 +1,5 @@
 'use strict';
-const CACHE='gestor-mobile-shell-v3';
+const CACHE='gestor-mobile-shell-v4';
 const ASSETS=['./','./index.html','./mobile.css','./device.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
